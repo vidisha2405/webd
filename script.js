00e812f6513f93cf129028c9c8f2440241dc2cc6
unknown <vidishaarora2405@gmail.com>
2020-01-08 20:13:38 +0530

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+<div class="form-group">
+                  <label for="PageTitle">Page Title</label>
+                  <input type="text" class="form-control" id="pageTitle">
+                </div>
+                <div class="form-group">
+                  <label for="PageBody">Page Body</label>
+                  <input type="text" class="form-control" id="PageBody">
+                </div>
